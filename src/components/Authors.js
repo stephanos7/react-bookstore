5f34e0b3b15151e791cadf5550df87b2583cd984
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-05-07 14:03:06 +0100

    add author component

---
+++ src/components/Authors.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from "../styles/Authors-styles";
+
+const Authors = () => {
+  return (
+    <div style={styles.root}>
+      <h4>Authors</h4>
+      <p>Jason Jungles</p>
+    </div>
+  )
+}
+
+export default Authors;
--- src/components/Book.js
+++ src/components/Book.js
@@ -2,11 +2,15 @@ import React from "react";
 
 import styles from "../styles/Book-styles";
 import Cover from "./Cover";
+import Authors from "./Authors";
 
 const Book = () => {
   return (
     <div style={styles.root}>
       <Cover />
+      <div style={{justifySelf: "center"}}>
+        <Authors />
+      </div>
     </div>
   )
 }
