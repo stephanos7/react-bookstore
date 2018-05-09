379c4f2cccb68188917a92599e50409aff18ab2c
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-05-09 23:09:44 +0100

    refactor to stateless components with methods toggling state properties

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,14 +4,20 @@ import styles from "../styles/Book-styles";
 import Cover from "./Cover";
 import BookDetails from "./BookDetails";
 
-const Book = (props) => {
-  const {coverImage, authorName} = props.book;
-  return (
-    <div style={styles.root}>
-      <Cover imageURL={coverImage}/>
-      <BookDetails authorName={authorName} />
-    </div>
-  )
+class Book extends React.Component {
+  handleClick = () => {
+    this.props.triggerIX();
+  }
+
+  render(){
+    const {coverImage, authorName} = this.props.book;
+    return (
+      <div style={styles.root} onClick={() => this.handleClick()}>
+        <Cover imageURL={coverImage}/>
+        <BookDetails authorName={authorName} />
+      </div>
+    )
+  }
 }
 
 export default Book;
--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -3,16 +3,27 @@ import React from "react";
 import Book from "./Book";
 import styles from "../styles/ListBooks-styles";
 
-const ListBooks = (props) => {
-  const {books} = props;
-  return (
-    <div style={styles.root}>
-      {books.map( book => (
-        <Book key={book.id} 
-              book={book}/>
-      ))}
-    </div>
-  )
+class ListBooks extends React.Component {
+  state = {
+    toggle : false
+  }
+
+  triggerIX = () => {
+    this.setState( (currentState) => ({toggle: !currentState.toggle}) );
+  }
+
+  render(){
+    const {books} = this.props;
+    return (
+      <div style={styles.root}>
+        {books.map( book => (
+          <Book key={book.id} 
+                book={book}
+                triggerIX={this.triggerIX}/>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default ListBooks;
