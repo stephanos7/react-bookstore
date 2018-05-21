31ca5758e0406e7ad43ff43470b0a4a84fed9b4c
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-05-21 14:04:54 +0100

    reference the grid dom node and get the number of columns property via the nodes computed style

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -5,17 +5,25 @@ import listStyles from "../styles/ListBooks-styles";
 import bookStyles from "../styles/Book-styles";
 
 class ListBooks extends React.Component {
+  selectBook = () => console.log("parent was selected :");
 
-  selectBook = () => console.log("parent was selected :")
-  
+
+  grid = React.createRef();
+
+  showRef = () => {
+    console.log(this.grid.current) 
+    const gridDOMnodeComputedSyle = window.getComputedStyle(this.grid.current).getPropertyValue("grid-template-columns");
+    console.log(gridDOMnodeComputedSyle)
+  }
+
   render(){
     const {books} = this.props;
     const dynamicStyle = books.selected ? bookStyles.selected : bookStyles.normal;
 
     return (
-      <div style={listStyles.root}>
+      <div style={listStyles.root} ref={this.grid} onChange={this.handleClick}>
         {books.map( (book, i) => (
-          <div style={dynamicStyle} key={i} onClick={() => this.selectBook()}>
+          <div style={dynamicStyle} key={i} onClick={() => this.showRef()}>
             <Book book={book}/>
           </div>))}
       </div>
